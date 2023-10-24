47fc8e210bf0256ee8b71165bb018ae3c0f0506f
Nils <nils.reinhardt@mailbox.tu-dresden.de>
2023-10-24 08:51:26 +0200

    Creation of DB Tables

---
+++ src/com_server/db_tables.py
@@ -0,0 +1,44 @@
+from sqlalchemy import create_engine, Column
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.sql.sqltypes import String, Integer, DateTime, Boolean
+from sqlalchemy_utils import database_exists, create_database
+import psycopg2
+import os
+
+
+HOST_IP = "localhost" 
+db_string = f"postgresql://paktikum:automatisierungstechnik@{HOST_IP}:5432/smarthome"
+engine = create_engine(db_string)
+
+if not database_exists(engine.url):
+    print("[DB-Manager] SmarthomeDB created")
+    create_database(engine.url)
+else:
+    print("[DB-Manager] SmarthomeDB already exist")
+    exit
+
+engine.connect()
+base = declarative_base()
+conn = engine.connect()
+Session = sessionmaker()
+
+
+class Sensor_logs(base):
+    __tablename__ = "sensor_logs"
+    id = Column(Integer, primary_key=True)
+    sensor_type = Column(String(100))
+    sid = Column(Integer)
+    time = Column(String(100))
+    value = Column(String(100))
+
+class Sensors(base):
+    __tablename__ = "sensors"
+    sid = Column(Integer, primary_key=True)
+    uuid = Column(Integer)
+    sensor_type = Column(String(100))
+    name = Column(String(100))
+    
+
+base.metadata.create_all(conn)
+print("[DB-Manager] SmarthomeDB Tables created")
